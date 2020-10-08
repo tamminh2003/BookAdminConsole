5b19c0fca009a00f416918202e20917776e60bfe
tamminh2003 <tamminh2003@gmail.com>
2020-10-09 10:21:40 +1100

    Create UserDAO.java

---
+++ src/mvccrudpackage/model/dao/UserDAO.java
@@ -0,0 +1,16 @@
+package mvccrudpackage.model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserDAO {
+
+}
